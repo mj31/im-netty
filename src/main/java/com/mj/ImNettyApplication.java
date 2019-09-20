6760459fa59bf754b6f9eb0ebe2c6b83c0cd7cbc
906625611@qq.com <Gcy12345678>
2019-09-20 17:43:01 +0800

    first commit

---
+++ src/main/java/com/mj/ImNettyApplication.java
@@ -0,0 +1,13 @@
+package com.mj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImNettyApplication.class, args);
+    }
+
+}
